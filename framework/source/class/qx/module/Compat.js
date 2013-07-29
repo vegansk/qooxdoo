c7cbea597638a8c81ebcbe258340787ee6e94f9b
danielwagner <daniel.wagner@1und1.de>
2013-07-29 14:19:13 +0200

    use qx.Desktop event layer

--- framework/source/class/qx/module/Compat.js
+++ framework/source/class/qx/module/Compat.js
@@ -1,9 +1,134 @@
-/**
- * require(qx.module.Manipulating)
- */
 qx.Bootstrap.define("qx.module.Compat", {
 
   statics : {
+    /*
+    ---------------------------------------------------------------------------
+      EVENT SUPPORT
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * Adds an event listener to the element.
+     *
+     * @param type {String} Name of the event
+     * @param listener {Function} Function to execute on event
+     * @param self {Object ? null} Reference to the 'this' variable inside
+     *         the event listener. When not given, the corresponding dispatcher
+     *         usually falls back to a default, which is the target
+     *         by convention. Note this is not a strict requirement, i.e.
+     *         custom dispatchers can follow a different strategy.
+     * @param capture {Boolean ? false} Whether capturing should be enabled
+     * @return {var} An opaque id, which can be used to remove the event listener
+     *         using the {@link #removeListenerById} method.
+     * @attach{qxWeb}
+     */
+    addListener : function(type, listener, self, capture)
+    {
+      //TODO: assertions
+      if (false && qx.core.Environment.get("qx.debug"))
+      {
+        var msg = "Failed to add event listener for type '" + type + "'" +
+          " to the target '" + this + "': ";
+
+        this.assertString(type, msg + "Invalid event type.");
+        this.assertFunction(listener, msg + "Invalid callback function");
+
+        if (self !== undefined) {
+          this.assertObject(self, "Invalid context for callback.")
+        }
+
+        if (capture !== undefined) {
+          this.assertBoolean(capture, "Invalid capture flag.");
+        }
+      }
+
+      if (this[0]) {
+        return qx.event.Registration.addListener(this[0], type, listener, self, capture);
+      }
+
+      if (!this.__eventValues) {
+        this.__eventValues = {};
+      }
+
+      if (capture == null) {
+        capture = false;
+      }
+
+      var unique = qx.event.Manager.getNextUniqueId();
+      var id = type + (capture ? "|capture|" : "|bubble|") + unique;
+
+      this.__eventValues[id] =
+      {
+        type : type,
+        listener : listener,
+        self : self,
+        capture : capture,
+        unique : unique
+      };
+
+      return id;
+    },
+
+
+    /**
+     * Removes an event listener from the element.
+     *
+     * @param type {String} Name of the event
+     * @param listener {Function} Function to execute on event
+     * @param self {Object} Execution context of given function
+     * @param capture {Boolean ? false} Whether capturing should be enabled
+     * @return {qx.html.Element} this object (for chaining support)
+     */
+    removeListener : function(type, listener, self, capture)
+    {
+      //TODO: assertions
+      if (false && qx.core.Environment.get("qx.debug"))
+      {
+        var msg = "Failed to remove event listener for type '" + type + "'" +
+          " from the target '" + this + "': ";
+
+        this.assertString(type, msg + "Invalid event type.");
+        this.assertFunction(listener, msg + "Invalid callback function");
+
+        if (self !== undefined) {
+          this.assertObject(self, "Invalid context for callback.")
+        }
+
+        if (capture !== undefined) {
+          this.assertBoolean(capture, "Invalid capture flag.");
+        }
+      }
+
+      if (this[0])
+      {
+        qx.event.Registration.removeListener(this[0], type, listener, self, capture);
+      }
+      else
+      {
+        var values = this.__eventValues;
+        var entry;
+
+        if (capture == null) {
+          capture = false;
+        }
+
+        for (var key in values)
+        {
+          entry = values[key];
+
+          // Optimized for performance: Testing references first
+          if (entry.listener === listener && entry.self === self && entry.capture === capture && entry.type === type)
+          {
+            delete values[key];
+            break;
+          }
+        }
+      }
+
+      return this;
+    },
+
+
 
     /*
     ---------------------------------------------------------------------------
@@ -293,8 +418,13 @@ qx.Bootstrap.define("qx.module.Compat", {
 
   defer : function(statics) {
     qxWeb.$attach({
-      "addListener" : qx.module.Event.on,
-      "removeListener" : qx.module.Event.off,
+      "addListener" : statics.addListener,
+      "removeListener" : statics.removeListener,
+
+      "capture" : function() {},
+      "releaseCapture" : function() {},
+      "activate" : function() {},
+
       "scrollChildIntoViewX": statics.scrollChildIntoViewX,
       "scrollChildIntoViewY": statics.scrollChildIntoViewY,
       "scrollToX" : qx.module.Manipulating.setScrollLeft,
