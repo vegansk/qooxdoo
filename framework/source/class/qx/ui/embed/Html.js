2dc51571a467f24181926d82f49ebe2ad82d4ba1
danielwagner <daniel.wagner@1und1.de>
2013-07-29 14:18:47 +0200

    fixed color reset

--- framework/source/class/qx/ui/embed/Html.js
+++ framework/source/class/qx/ui/embed/Html.js
@@ -216,7 +216,7 @@ qx.Class.define("qx.ui.embed.Html",
       if (value) {
         this.getContentElement().setStyle("color", qx.theme.manager.Color.getInstance().resolve(value));
       } else {
-        this.getContentElement().removeStyle("color");
+        this.getContentElement().setStyle("color", "black");
       }
     }
   }
