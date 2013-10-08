2fd084f52c5c8fb0c8c73bfda417a0abddba3ae2
wittemann <martin.wittemann@1und1.de>
2013-10-08 13:31:20 +0200

    [BUG #7710] Removed desktop app and table demo.

--- application/domains/source/class/domains/Application.js
+++ application/domains/source/class/domains/Application.js
@@ -45,48 +45,6 @@ qx.Class.define("domains.Application",
       // Call super class
       this.base(arguments);
 
-      var desktop = document.getElementById("desktop");
-      var inlineIsle = new qx.ui.root.Inline(desktop, true, true);
-      inlineIsle.setLayout(new qx.ui.layout.Canvas());
-
-      var toolbar = new qx.ui.toolbar.ToolBar();
-      inlineIsle.add(toolbar, {left: 0, right: 0});
-
-
-      var menu = new qx.ui.menu.Menu();
-      menu.add(new qx.ui.menu.Button("abcabcabc"));
-      menu.add(new qx.ui.menu.Button("defdef"));
-
-      var newButton = new qx.ui.toolbar.MenuButton("Neu");
-      newButton.setMenu(menu);
-      toolbar.add(newButton);
-
-      var typeButton = new qx.ui.toolbar.MenuButton("Domain-Typ");
-      toolbar.add(typeButton);
-
-      var dnsButton = new qx.ui.toolbar.MenuButton("DNS");
-      toolbar.add(dnsButton);
-
-      var delButton = new qx.ui.toolbar.Button("Subdomain löschen");
-      toolbar.add(delButton);
-
-      var moveButton = new qx.ui.toolbar.MenuButton("Domain-Umzüge");
-      toolbar.add(moveButton);
-
-      var sitemapButton = new qx.ui.toolbar.MenuButton("Google Sitemaps");
-      toolbar.add(sitemapButton);
-
-      var infoButton = new qx.ui.toolbar.Button("Info");
-      toolbar.add(infoButton);
-
-      var filterButton = new qx.ui.toolbar.Button("Filter");
-      toolbar.add(filterButton);
-
-      var searchButton = new qx.ui.toolbar.Button("Suche");
-      toolbar.add(searchButton);
-
-
-
       // Website widget
       var root = document.body;
       var arrow = "down.gif";
@@ -102,7 +60,7 @@ qx.Class.define("domains.Application",
 
       // buttons
       toolbar.getChildren().button().eq(0).setMenu(menu);
-      toolbar.getChildren(".qx-menu-button").setIcon(arrow);
+      toolbar.getChildren(".qx-menu-button").setIcon(arrow); // TODO use ▾
 
       toolbar.getChildren().on("click", function() {
         console.log("Click on", this.getLabel());
@@ -114,45 +72,45 @@ qx.Class.define("domains.Application",
       disabledCollection.setEnabled(false);
 
 
-      // table stuff
-      var data = [
-        {name: "abc", type: "bcd", kind: "def", status: "efg"},
-        {name: "cbc", type: "bcd", kind: "def", status: "efg"},
-        {name: "bbc", type: "bcd", kind: "def", status: "efg"},
-        {name: "123", type: "234", kind: "345", status: "456"}
-      ];
-
-      var template = q("#domains tbody").getHtml();
-      q("#domains tbody").getChildren().remove();
-      for (var i = 0; i < data.length; i++) {
-        var html = q.template.render(template, data[i]);
-        q.create(html).appendTo("#domains tbody");
-      }
-
-      // sorting
-      var asc = true;
-      q("#domain-names").on("click", function() {
-        asc = !asc;
-        this.getSiblings().setHtml(asc ? "↓" : "↑");
-        q("#domains tbody").getChildren().sort(function(a, b) {
-          var txtA = q(a).getChildren()[1].textContent;
-          var txtB = q(b).getChildren()[1].textContent
-          return txtA === txtB ? 0 : asc ? txtA > txtB : txtA < txtB;
-        }).appendTo("#domains tbody");
-      });
-
-      // checkbox
-      q("thead").find("input").on("change", function(e) {
-        q("tbody").find("input").setAttribute("checked", e.target.checked);
-      });
-
-      q("#domains").find("[type=checkbox]").on("change", function(e) {
-        var checked = false;
-        q("#domains").find("[type=checkbox]").forEach(function(item) {
-          checked = checked || q(item).getAttribute("checked");
-        });
-        disabledCollection.setEnabled(checked);
-      });
+      // // table stuff
+      // var data = [
+      //   {name: "abc", type: "bcd", kind: "def", status: "efg"},
+      //   {name: "cbc", type: "bcd", kind: "def", status: "efg"},
+      //   {name: "bbc", type: "bcd", kind: "def", status: "efg"},
+      //   {name: "123", type: "234", kind: "345", status: "456"}
+      // ];
+      // 
+      // var template = q("#domains tbody").getHtml();
+      // q("#domains tbody").getChildren().remove();
+      // for (var i = 0; i < data.length; i++) {
+      //   var html = q.template.render(template, data[i]);
+      //   q.create(html).appendTo("#domains tbody");
+      // }
+      // 
+      // // sorting
+      // var asc = true;
+      // q("#domain-names").on("click", function() {
+      //   asc = !asc;
+      //   this.getSiblings().setHtml(asc ? "↓" : "↑");
+      //   q("#domains tbody").getChildren().sort(function(a, b) {
+      //     var txtA = q(a).getChildren()[1].textContent;
+      //     var txtB = q(b).getChildren()[1].textContent
+      //     return txtA === txtB ? 0 : asc ? txtA > txtB : txtA < txtB;
+      //   }).appendTo("#domains tbody");
+      // });
+      // 
+      // // checkbox
+      // q("thead").find("input").on("change", function(e) {
+      //   q("tbody").find("input").setAttribute("checked", e.target.checked);
+      // });
+      // 
+      // q("#domains").find("[type=checkbox]").on("change", function(e) {
+      //   var checked = false;
+      //   q("#domains").find("[type=checkbox]").forEach(function(item) {
+      //     checked = checked || q(item).getAttribute("checked");
+      //   });
+      //   disabledCollection.setEnabled(checked);
+      // });
 
 
       // rating
