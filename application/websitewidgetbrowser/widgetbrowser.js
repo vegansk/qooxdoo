cc82da4fe58a2f7569d0c8359dbbcd62e5dcfd84
danielwagner <daniel.wagner@1und1.de>
2013-10-14 16:13:52 +0200

    [BUG #7710] Disable/enable all demo widgets at once

--- application/websitewidgetbrowser/widgetbrowser.js
+++ application/websitewidgetbrowser/widgetbrowser.js
@@ -1,16 +1,23 @@
 q.ready(function() {
   //qxWeb.initWidgets();
 
+  /**
+   * Disable/enable all widgets on each tab
+   */
+  var onDisable = function() {
+    var enabled = !this.getAttribute("checked");
+    q("#content > ul > .qx-tab-button")._forEachElementWrapped(function(button) {
+      var selector = button.getData("qx-tab-page");
+      var widgets = q(selector).getChildren("*[qx-class]");
+      if (widgets.length > 0) {
+        widgets.setEnabled(enabled);
+      }
+    });
+  };
+  q(".disable input").on("change", onDisable);
+
   var menu = q("#menu").addClass("qx-menu").appendTo(document.body).hide();
   q("#menu-button").setMenu(menu);
 
-  q(".disable input").on("change", onDisable);
 });
 
-var onDisable = function() {
-  var enabled = !this.getAttribute("checked");
-  q("#content > ul > .qx-tab-button")._forEachElementWrapped(function(button) {
-    var selector = button.getData("qx-tab-page");
-    q(selector).getChildren("*[qx-class]").setEnabled(enabled);
-  });
-};
