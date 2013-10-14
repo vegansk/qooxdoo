c0a5860a6da3d549494a2ab94e1961ed6851ea3a
danielwagner <daniel.wagner@1und1.de>
2013-10-14 16:17:11 +0200

    [BUG #7710] select tab by URL hash

--- application/websitewidgetbrowser/widgetbrowser.js
+++ application/websitewidgetbrowser/widgetbrowser.js
@@ -1,5 +1,4 @@
 q.ready(function() {
-  //qxWeb.initWidgets();
 
   /**
    * Disable/enable all widgets on each tab
@@ -14,10 +13,50 @@ q.ready(function() {
       }
     });
   };
+
+  /**
+   * Select the tab with the given title
+   * @param title {String} Tab (button) title
+   */
+  var selectTab = function(title) {
+    var tabs = q("#content > ul > .qx-tab-button");
+    var selectedTab = tabs.has("button:contains(" + title + ")");
+    if (selectedTab.length > 0) {
+      var index = tabs.indexOf(selectedTab);
+      q("#content").select(index);
+    }
+  };
+  /**
+   * Set the title of the tab with the given index as URL hash
+   * @param index {Number} tab index
+   */
+  var onChangeSelected = function(index) {
+    var buttonText = q("#content > ul > .qx-tab-button").eq(index).getChildren("button").getHtml();
+    location.hash = buttonText;
+  };
+
+
+  qxWeb.initWidgets();
+  q("#content")
+  .on("changeSelected", onChangeSelected)
   q(".disable input").on("change", onDisable);
+
 
   var menu = q("#menu").addClass("qx-menu").appendTo(document.body).hide();
   q("#menu-button").setMenu(menu);
 
+
+  // select tab by URL hash or select the tabs widget's default
+  setTimeout(function() {
+    var selected;
+    if (location.hash.length > 0) {
+      selected = location.hash.substr(1);
+    } else {
+      selected = q("#content").tabs().find(".qx-tab-button-active").getChildren("button").getHtml();
+    }
+    selectTab(selected);
+  }, 100);
+
 });
+
 
